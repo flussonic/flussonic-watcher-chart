d0f9be4fc989be5ee697991f17f925c8eb5e3471
Max Lapshin <max@maxidoors.ru>
2024-02-26 20:47:00 +0300

    Almost working ingress #37743

--- mp-start.sh
+++ mp-start.sh
@@ -24,7 +24,8 @@ multipass launch --name watcher --cpus 1 --memory 1024M --disk 5G focal
 multipass launch --name streamer1 --cpus 1 --memory 1024M --disk 5G focal
 multipass launch --name streamer2 --cpus 1 --memory 1024M --disk 5G focal
 
-multipass exec watcher -- sudo /bin/sh -c 'curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--disable traefik" INSTALL_K3S_VERSION="v1.25.11+k3s1" sh -'
+# multipass exec watcher -- sudo /bin/sh -c 'curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--disable traefik" INSTALL_K3S_VERSION="v1.25.11+k3s1" sh -'
+multipass exec watcher -- sudo /bin/sh -c 'curl -sfL https://get.k3s.io | sh -'
 
 token=$(multipass exec watcher sudo cat /var/lib/rancher/k3s/server/node-token)
 plane_ip=$(multipass info watcher | grep -i ip | awk '{print $2}')
@@ -35,7 +36,7 @@ multipass exec streamer2 -- sudo /bin/sh -c "curl -sfL https://get.k3s.io | K3S_
 multipass exec watcher sudo cat /etc/rancher/k3s/k3s.yaml |sed "s/127.0.0.1/${plane_ip}/" > k3s.yaml
 export KUBECONFIG=`pwd`/k3s.yaml
 
-kubectl label nodes watcher flussonic.com/streamer=true
+kubectl label nodes watcher flussonic.com/database=true
 kubectl label nodes streamer1 flussonic.com/streamer=true
 kubectl label nodes streamer2 flussonic.com/streamer=true
 
@@ -43,25 +44,10 @@ kubectl create secret generic flussonic-license --from-literal=license_key="${LI
 
 kubectl create secret generic watcher-admin --from-literal=login="${LOGIN}" --from-literal=pass="${PASS}"
 
-exit 0
 
-kubectl apply -f 01-postgres.yaml
-kubectl apply -f 02-streamer.yaml
-kubectl apply -f 03-central.yaml
-kubectl apply -f 04-redis.yaml
-kubectl apply -f 05-watcher-firstrun.yaml
+# watcher_ip=$(multipass info watcher | grep -i ip | awk '{print $2}')
 
-echo "Waiting for Watcher firstrun (up to 3 minutes)"
-kubectl wait --for=condition=complete --timeout=180s job.batch/watcher-firstrun
+# echo "Waiting for Watcher http://${watcher_ip} to start" 
+# kubectl wait --for=condition=Ready pod/watcher-0
 
-kubectl apply -f 06-watcher.yaml
-kubectl apply -f 07-watcher-services.yaml
-
-
-
-watcher_ip=$(multipass info watcher | grep -i ip | awk '{print $2}')
-
-echo "Waiting for Watcher http://${watcher_ip} to start" 
-kubectl wait --for=condition=Ready pod/watcher-0
-
-echo "Watcher ready: http://${watcher_ip}/vsaas  with login/pass: ${LOGIN} ${PASS}"
+# echo "Watcher ready: http://${watcher_ip}/vsaas  with login/pass: ${LOGIN} ${PASS}"
